a4082b8a73d0bfa690c0106ea8d8b401cac3e8dc
Allan W <51011089+ertyertyerty@users.noreply.github.com>
2024-02-08 22:51:38 +0000

    Solution for chapter 2 challenge

--- 02/02_09/02_09b/main.py
+++ 02/02_09/02_09b/main.py
@@ -1,4 +1,8 @@
 def find_second_smallest(my_list):
-    return 0
+    return None if len(my_list) <= 1 else sorted(my_list)[-2]
 
 print(find_second_smallest([5, 8, 3, 2, 6]))
+print(find_second_smallest([5]))
+print(find_second_smallest([]))
+print(find_second_smallest([5, 8, 3, 2, 6, 33]))
+print(find_second_smallest(['person', 'woman', 'man', 'camera', 'tv']))
