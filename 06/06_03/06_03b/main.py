ae8a534c80e020a9e860627ea529efd2aed8b8b3
Allan W <51011089+ertyertyerty@users.noreply.github.com>
2024-02-09 06:16:31 +0000

    Chapter 6 challenge solution

--- 06/06_03/06_03b/main.py
+++ 06/06_03/06_03b/main.py
@@ -1,1 +1,29 @@
 from collections import deque
+
+def check_matching_parentheses(sample):
+    dubdeque = deque(sample)
+    count = 0
+    while len(dubdeque) > 0:
+        leftchar = dubdeque.popleft()
+        # print(leftchar)
+        if leftchar == '(':
+            count = count + 1
+        if leftchar == ')':
+            count = count - 1
+        if count < 0:
+            return False
+    if count == 0:
+        return True
+    else:
+        return False
+
+print(f"Answer: {check_matching_parentheses('((sdlf))ksdf')}")
+print(check_matching_parentheses("()"))
+print(check_matching_parentheses("(hi there)"))
+print(check_matching_parentheses("(hell)o"))
+print(check_matching_parentheses("((linkedin)) learning"))
+
+print(check_matching_parentheses("(hi(there"))
+print(check_matching_parentheses("()ok)"))
+print(check_matching_parentheses("((increment)"))
+print(check_matching_parentheses(")linkedin()"))
