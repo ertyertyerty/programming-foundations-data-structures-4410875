fbb30d27d099f0965592d4178ca42d94be8ea774
Allan W <51011089+ertyertyerty@users.noreply.github.com>
2024-02-09 04:34:28 +0000

    Chapter 4 challenge solution

--- 04/04_05/04_05b/main.py
+++ 04/04_05/04_05b/main.py
@@ -1,5 +1,13 @@
-def has_unique_characters(data):
+def has_unique_characters_1(data):
+    charset = set([])
+    for x in data:
+        charset.add(x)
+    if len(data) == len(charset):
+        return True
     return False
+
+def has_unique_characters(data):
+    return len(set(data)) == len(data)
 
 print(has_unique_characters('sample'))
 print(has_unique_characters('hello world'))
