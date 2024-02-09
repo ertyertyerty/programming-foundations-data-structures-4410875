20d2c5aff6b2ffb3c98816cf4d4f945aa178196a
Allan W <51011089+ertyertyerty@users.noreply.github.com>
2024-02-09 00:26:44 +0000

    Chapter 3 challenge solution

--- 02/02_09/02_09b/main.py
+++ 02/02_09/02_09b/main.py
@@ -1,5 +1,5 @@
 def find_second_smallest(my_list):
-    return None if len(my_list) <= 1 else sorted(my_list)[-2]
+    return None if len(my_list) <= 1 else sorted(my_list)[1]
 
 print(find_second_smallest([5, 8, 3, 2, 6]))
 print(find_second_smallest([5]))
--- 03/03_04/03_04b/main.py
+++ 03/03_04/03_04b/main.py
@@ -8,7 +8,16 @@ user_preferences = {
 
 
 def update_preferences(user_pref):
-    return {}
+    return_pref = {}
+    for key, value in user_pref.items():
+        if value != None:
+            return_pref[key] = value
+    return return_pref
 
+
+print(update_preferences(user_preferences))
+
+user_preferences = {
+}
 
 print(update_preferences(user_preferences))
